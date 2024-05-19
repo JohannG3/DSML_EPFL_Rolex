a621c2fe070ad82a2bb9aa99caf8e8a94a1d3b30
JohannG3 <100433535+JohannG3@users.noreply.github.com>
2024-05-19 19:46:25 +0200

    Update Streamlit_app_EPFL_Rolex_LR.py

--- Streamlit_app_EPFL_Rolex_LR.py
+++ Streamlit_app_EPFL_Rolex_LR.py
@@ -24,31 +24,6 @@ def get_synonyms(word):
     }
     return synonyms.get(word, [])
 
-# Téléchargement et chargement du modèle
-#response = requests.get(url)
-#model = load(BytesIO(response.content))
-
-# Clé API pour Dicolink
-#api_key = 'YOUR_DICOLINK_API_KEY'
-
-#def get_synonyms(word):
-    # Fonction pour obtenir les synonymes d'un mot en français
-    #api_url = f"https://api.dicolink.com/v1/mot/{word}/synonymes?limite=5&api_key={api_key}"
-    #response = requests.get(api_url)
-    #data = response.json()
-    #synonyms = [item['mot'] for item in data] if isinstance(data, list) else "Aucun synonyme trouvé"
-    #return ", ".join(synonyms)
-
-def get_synonyms(word):
-    # Simuler une fonction de récupération de synonymes
-    synonyms = {
-        "manger": ["consommer", "dévorer", "ingérer"],
-        "pomme": ["fruit"],
-        "perdu": ["égaré", "disparu", "paumé"]
-    }
-    # Retourner une liste vide si le mot n'est pas trouvé
-    return synonyms.get(word, [])
-
 def reset_form():
     st.session_state.sentence = ""
     st.session_state.improved = ""
@@ -65,14 +40,8 @@ if st.button('Predict', key='predict'):
         synonyms = get_synonyms(word)
         if synonyms:
             st.write(f"Synonyms for '{word}': {', '.join(synonyms)}")
-    
-    # Afficher les synonymes pour chaque mot
-    #words = sentence.split()
-    #for word in words:
-        #synonyms = get_synonyms(word)
-        #st.write(f"Synonymes pour '{word}' : {synonyms}")
-    
-    # Sauvegarder la prédiction actuelle pour la comparaison ultérieure
+
+    # Sauvegarder la prédiction initiale pour comparaison ultérieure
     st.session_state.current_prediction = prediction
 
 # Interaction pour améliorer la phrase
@@ -91,3 +60,4 @@ if 'current_prediction' in st.session_state:
                 reset_form()
         else:
             st.error("The difficulty level has not increased. Try again!")
+
