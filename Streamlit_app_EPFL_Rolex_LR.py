91d7b25fe2b23ae7f1572dcc17af2675e9fb7814
JohannG3 <100433535+JohannG3@users.noreply.github.com>
2024-05-19 20:05:11 +0200

    Update Streamlit_app_EPFL_Rolex_LR.py

--- Streamlit_app_EPFL_Rolex_LR.py
+++ Streamlit_app_EPFL_Rolex_LR.py
@@ -24,42 +24,40 @@ def get_synonyms(word):
     }
     return synonyms.get(word, [])
 
-# Nouvelle session de prédiction
-def new_prediction_session():
-    with st.container():
-        sentence = st.text_input("Sentence", key=f'sentence_{st.session_state.session_count}')
-        if st.button('Predict', key=f'predict_{st.session_state.session_count}'):
-            if sentence:
-                prediction = st.session_state.model.predict([sentence])[0]
-                st.write(f"The predicted difficulty level for this sentence is: {prediction}")
-                
-                words = sentence.split()
-                for word in words:
-                    synonyms = get_synonyms(word)
-                    if synonyms:
-                        st.write(f"Synonyms for '{word}': {', '.join(synonyms)}")
-                
-                st.session_state['current_prediction'] = prediction
+# Gestion des cycles de phrases
+if 'cycle_count' not in st.session_state:
+    st.session_state.cycle_count = 0
 
-        # Interaction pour améliorer la phrase
-        if 'current_prediction' in st.session_state:
-            improved_sentence = st.text_input("Improve your sentence to increase the difficulty level:", key=f'improved_{st.session_state.session_count}')
-            if st.button('Submit the improved sentence', key=f"submit_improved_{st.session_state.session_count}"):
-                new_prediction = st.session_state.model.predict([improved_sentence])[0]
-                st.write(f"The new predicted difficulty level for your improved sentence is: {new_prediction}")
-                
-                if new_prediction > st.session_state['current_prediction']:
-                    st.success("Congratulations! The difficulty level of your sentence has increased.")
-                else:
-                    st.error("The difficulty level has not increased. Try again!")
+def new_sentence_cycle():
+    st.session_state.cycle_count += 1
 
-# Initialisation du compteur de sessions s'il n'existe pas
-if 'session_count' not in st.session_state:
-    st.session_state.session_count = 0
+sentence = st.text_input("Sentence", key=f"sentence_{st.session_state.cycle_count}")
 
-# Bouton pour démarrer une nouvelle session de prédiction
-if st.button("Enter a new sentence"):
-    st.session_state.session_count += 1
-    new_prediction_session()
-else:
-    new_prediction_session()
+if st.button('Predict', key=f"predict_{st.session_state.cycle_count}"):
+    prediction = st.session_state.model.predict([sentence])[0]
+    st.write(f"The predicted difficulty level for this sentence is: {prediction}")
+    
+    words = sentence.split()
+    for word in words:
+        synonyms = get_synonyms(word)
+        if synonyms:
+            st.write(f"Synonyms for '{word}': {', '.join(synonyms)}")
+
+    # Sauvegarder la prédiction initiale pour comparaison ultérieure
+    st.session_state.current_prediction = prediction
+
+# Interaction pour améliorer la phrase
+if 'current_prediction' in st.session_state:
+    improved_sentence = st.text_input("Improve your sentence to increase the difficulty level:", key=f"improved_{st.session_state.cycle_count}")
+
+    if st.button('Submit the improved sentence', key=f"submit_improved_{st.session_state.cycle_count}"):
+        new_prediction = st.session_state.model.predict([improved_sentence])[0]
+        st.write(f"The new predicted difficulty level for your improved sentence is: {new_prediction}")
+        
+        if new_prediction > st.session_state.current_prediction:
+            st.success("Congratulations! The difficulty level of your sentence has increased.")
+            st.session_state.current_prediction = new_prediction  # Mise à jour de la prédiction actuelle
+            if st.button('Enter a new sentence', key=f"new_sentence_{st.session_state.cycle_count}"):
+                new_sentence_cycle()
+        else:
+            st.error("The difficulty level has not increased. Try again!")
