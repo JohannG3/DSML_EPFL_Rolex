d3a7e652f9a035ea8924a0014b7b81425ab83dce
JohannG3 <100433535+JohannG3@users.noreply.github.com>
2024-05-19 21:01:52 +0200

    Update Streamlit_app_EPFL_Rolex_LR.py

--- Streamlit_app_EPFL_Rolex_LR.py
+++ Streamlit_app_EPFL_Rolex_LR.py
@@ -3,18 +3,6 @@ import requests
 from joblib import load
 from io import BytesIO
 
-# Titre de l'application
-st.title('Predicting sentence difficulty in French')
-
-# Description
-st.write("Enter a sentence in French to predict its difficulty level and get synonyms to enrich your vocabulary.")
-
-# Chargement du modèle si pas déjà chargé
-if 'model' not in st.session_state:
-    url = 'https://github.com/JohannG3/DSML_EPFL_Rolex/blob/main/french_difficulty_predictor_model.joblib?raw=true'
-    response = requests.get(url)
-    st.session_state.model = load(BytesIO(response.content))
-
 # Fonction pour obtenir des synonymes
 def get_synonyms(word):
     synonyms = {
@@ -25,17 +13,27 @@ def get_synonyms(word):
     return synonyms.get(word, [])
 
 # Gestion des cycles d'interaction
-def start_new_cycle():
-    # Réinitialiser les clés pertinentes dans le session state pour un nouveau cycle
-    keys_to_delete = [key for key in st.session_state.keys() if key.startswith('cycle_')]
-    for key in keys_to_delete:
-        del st.session_state[key]
-    st.session_state['cycle'] = st.session_state.get('cycle', 0) + 1
+def handle_new_cycle():
+    if 'cycle' in st.session_state:
+        st.session_state.cycle += 1
+    else:
+        st.session_state.cycle = 0
 
-if 'cycle' not in st.session_state:
-    st.session_state['cycle'] = 0
+if 'reset' in st.session_state and st.session_state.reset:
+    handle_new_cycle()
+    st.session_state.reset = False
 
-cycle_key = f"cycle_{st.session_state['cycle']}"
+cycle_key = f"cycle_{st.session_state.get('cycle', 0)}"
+
+# Chargement du modèle si pas déjà chargé
+if 'model' not in st.session_state:
+    url = 'https://github.com/JohannG3/DSML_EPFL_Rolex/blob/main/french_difficulty_predictor_model.joblib?raw=true'
+    response = requests.get(url)
+    st.session_state.model = load(BytesIO(response.content))
+
+# Titre et description de l'application
+st.title('Predicting sentence difficulty in French')
+st.write("Enter a sentence in French to predict its difficulty level and get synonyms to enrich your vocabulary.")
 
 # Entrée de la phrase initiale
 sentence = st.text_input("Sentence", key=f"sentence_{cycle_key}")
@@ -65,6 +63,6 @@ if f"current_prediction_{cycle_key}" in st.session_state:
         if new_prediction > st.session_state[f"current_prediction_{cycle_key}"]:
             st.success("Congratulations! The difficulty level of your sentence has increased.")
             if st.button('Enter a new sentence', key=f"new_sentence_{cycle_key}"):
-                start_new_cycle()  # Reset everything for a new cycle
+                st.session_state.reset = True  # Flag to trigger the reset on rerun
         else:
             st.error("The difficulty level has not increased. Try again!")
