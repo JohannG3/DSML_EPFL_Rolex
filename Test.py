caf9237818f94be70098b3321b28a9e7964afd87
JohannG3 <100433535+JohannG3@users.noreply.github.com>
2024-05-20 20:53:19 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,132 +1,119 @@
-"""
 import streamlit as st
 import requests
 from joblib import load
 from io import BytesIO
 
-# Titre de l'application
-st.title('Predicting sentence difficulty in French with Synonym Enhancement')
+st.markdown("""
+    <style>
+    h1 {
+        color: pink;
+        font-size: 48px;
+    }
+    h2 {
+        color: blue;
+        font-size: 36px;
+    }
+    .reportview-container .markdown-text-container {
+        font-family: monospace;
+    }
+    .sidebar .sidebar-content {
+        background-color: pink;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
-# Description
-st.write("Enter a sentence in French to predict its difficulty level, translate to English, get synonyms in English, and translate them back to French.")
+english_stopwords = ['able', 'about', 'above', 'abroad', 'according', 'accordingly', 'across', 'actually', 'adj', 'after', 'afterwards', 'again', 'against', 'ago', 'ahead', "ain't", 'all', 'allow', 'allows', 'almost', 'alone', 'along', 'alongside', 'already', 'also', 'although', 'always', 'am', 'amid', 'amidst', 'among', 'amongst', 'an', 'and', 'another', 'any', 'anybody', 'anyhow', 'anyone', 'anything', 'anyway', 'anyways', 'anywhere', 'apart', 'appear', 'appreciate', 'appropriate', 'are', "aren't", 'around', 'as', "a's", 'aside', 'ask', 'asking', 'associated', 'at', 'available', 'away', 'awfully', 'back', 'backward', 'backwards', 'be', 'became', 'because', 'become', 'becomes', 'becoming', 'been', 'before', 'beforehand', 'begin', 'behind', 'being', 'believe', 'below', 'beside', 'besides', 'best', 'better', 'between', 'beyond', 'both', 'brief', 'but', 'by', 'came', 'can', 'cannot', 'cant', "can't", 'caption', 'cause', 'causes', 'certain', 'certainly', 'changes', 'clearly', "c'mon", 'co', 'co.', 'com', 'come', 'comes', 'concerning', 'consequently', 'consider', 'considering', 'contain', 'containing', 'contains', 'corresponding', 'could', "couldn't", 'course', "c's", 'currently', 'dare', "daren't", 'definitely', 'described', 'despite', 'did', "didn't", 'different', 'directly', 'do', 'does', "doesn't", 'doing', 'done', "don't", 'down', 'downwards', 'during', 'each', 'edu', 'eg', 'eight', 'eighty', 'either', 'else', 'elsewhere', 'end', 'ending', 'enough', 'entirely', 'especially', 'et', 'etc', 'even', 'ever', 'evermore', 'every', 'everybody', 'everyone', 'everything', 'everywhere', 'ex', 'exactly', 'example', 'except', 'fairly', 'far', 'farther', 'few', 'fewer', 'fifth', 'first', 'five', 'followed', 'following', 'follows', 'for', 'forever', 'former', 'formerly', 'forth', 'forward', 'found', 'four', 'from', 'further', 'furthermore', 'get', 'gets', 'getting', 'given', 'gives', 'go', 'goes', 'going', 'gone', 'got', 'gotten', 'greetings', 'had', "hadn't", 'half', 'happens', 'hardly', 'has', "hasn't", 'have', "haven't", 'having', 'he', "he'd", "he'll", 'hello', 'help', 'hence', 'her', 'here', 'hereafter', 'hereby', 'herein', "here's", 'hereupon', 'hers', 'herself', "he's", 'hi', 'him', 'himself', 'his', 'hither', 'hopefully', 'how', 'howbeit', 'however', 'hundred', "i'd", 'ie', 'if', 'ignored', "i'll", "i'm", 'immediate', 'in', 'inasmuch', 'inc', 'inc.', 'indeed', 'indicate', 'indicated', 'indicates', 'inner', 'inside', 'insofar', 'instead', 'into', 'inward', 'is', "isn't", 'it', "it'd", "it'll", 'its', "it's", 'itself', "i've", 'just', 'k', 'keep', 'keeps', 'kept', 'know', 'known', 'knows', 'last', 'lately', 'later', 'latter', 'latterly', 'least', 'less', 'lest', 'let', "let's", 'like', 'liked', 'likely', 'likewise', 'little', 'look', 'looking', 'looks', 'low', 'lower', 'ltd', 'made', 'mainly', 'make', 'makes', 'many', 'may', 'maybe', "mayn't", 'me', 'mean', 'meantime', 'meanwhile', 'merely', 'might', "mightn't", 'mine', 'minus', 'miss', 'more', 'moreover', 'most', 'mostly', 'mr', 'mrs', 'much', 'must', "mustn't", 'my', 'myself', 'name', 'namely', 'nd', 'near', 'nearly', 'necessary', 'need', "needn't", 'needs', 'neither', 'never', 'neverf', 'neverless', 'nevertheless', 'new', 'next', 'nine', 'ninety', 'no', 'nobody', 'non', 'none', 'nonetheless', 'noone', 'no-one', 'nor', 'normally', 'not', 'nothing', 'notwithstanding', 'novel', 'now', 'nowhere', 'obviously', 'of', 'off', 'often', 'oh', 'ok', 'okay', 'old', 'on', 'once', 'one', 'ones', "one's", 'only', 'onto', 'opposite', 'or', 'other', 'others', 'otherwise', 'ought', "oughtn't", 'our', 'ours', 'ourselves', 'out', 'outside', 'over', 'overall', 'own', 'particular', 'particularly', 'past', 'per', 'perhaps', 'placed', 'please', 'plus', 'possible', 'presumably', 'probably', 'provided', 'provides', 'que', 'quite', 'qv', 'rather', 'rd', 're', 'really', 'reasonably', 'recent', 'recently', 'regarding', 'regardless', 'regards', 'relatively', 'respectively', 'right', 'round', 'said', 'same', 'saw', 'say', 'saying', 'says', 'second', 'secondly', 'see', 'seeing', 'seem', 'seemed', 'seeming', 'seems', 'seen', 'self', 'selves', 'sensible', 'sent', 'serious', 'seriously', 'seven', 'several', 'shall', "shan't", 'she', "she'd", "she'll", "she's", 'should', "shouldn't", 'since', 'six', 'so', 'some', 'somebody', 'someday', 'somehow', 'someone', 'something', 'sometime', 'sometimes', 'somewhat', 'somewhere', 'soon', 'sorry', 'specified', 'specify', 'specifying', 'still', 'sub', 'such', 'sup', 'sure', 'take', 'taken', 'taking', 'tell', 'tends', 'th', 'than', 'thank', 'thanks', 'thanx', 'that', "that'll", 'thats', "that's", "that've", 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'thence', 'there', 'thereafter', 'thereby', "there'd", 'therefore', 'therein', "there'll", "there're", 'theres', "there's", 'thereupon', "there've", 'these', 'they', "they'd", "they'll", "they're", "they've", 'thing', 'things', 'think', 'third', 'thirty', 'this', 'thorough', 'thoroughly', 'those', 'though', 'three', 'through', 'throughout', 'thru', 'thus', 'till', 'to', 'together', 'too', 'took', 'toward', 'towards', 'tried', 'tries', 'truly', 'try', 'trying', "t's", 'twice', 'two', 'un', 'under', 'underneath', 'undoing', 'unfortunately', 'unless', 'unlike', 'unlikely', 'until', 'unto', 'up', 'upon', 'upwards', 'us', 'use', 'used', 'useful', 'uses', 'using', 'usually', 'v', 'value', 'various', 'versus', 'very', 'via', 'viz', 'vs', 'want', 'wants', 'was', "wasn't", 'way', 'we', "we'd", 'welcome', 'well', "we'll", 'went', 'were', "we're", "weren't", "we've", 'what', 'whatever', "what'll", "what's", "what've", 'when', 'whence', 'whenever', 'where', 'whereafter', 'whereas', 'whereby', 'wherein', "where's", 'whereupon', 'wherever', 'whether', 'which', 'whichever', 'while', 'whilst', 'whither', 'who', "who'd", 'whoever', 'whole', "who'll", 'whom', 'whomever', "who's", 'whose', 'why', 'will', 'willing', 'wish', 'with', 'within', 'without', 'wonder', "won't", 'would', "wouldn't", 'yes', 'yet', 'you', "you'd", "you'll", 'your', "you're", 'yours', 'yourself', 'yourselves', "you've", 'zero', 'a', "how's", 'i', "when's", "why's", 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'uucp', 'w', 'x', 'y', 'z', 'I', 'www', 'amount', 'bill', 'bottom', 'call', 'computer', 'con', 'couldnt', 'cry', 'de', 'describe', 'detail', 'due', 'eleven', 'empty', 'fifteen', 'fifty', 'fill', 'find', 'fire', 'forty', 'front', 'full', 'give', 'hasnt', 'herse', 'himse', 'interest', 'itse”', 'mill', 'move', 'myse”', 'part', 'put', 'show', 'side', 'sincere', 'sixty', 'system', 'ten', 'thick', 'thin', 'top', 'twelve', 'twenty', 'abst', 'accordance', 'act', 'added', 'adopted', 'affected', 'affecting', 'affects', 'ah', 'announce', 'anymore', 'apparently', 'approximately', 'aren', 'arent', 'arise', 'auth', 'beginning', 'beginnings', 'begins', 'biol', 'briefly', 'ca', 'date', 'ed', 'effect', 'et-al', 'ff', 'fix', 'gave', 'giving', 'heres', 'hes', 'hid', 'home', 'id', 'im', 'immediately', 'importance', 'important', 'index', 'information', 'invention', 'itd', 'keys', 'kg', 'km', 'largely', 'lets', 'line', "'ll", 'means', 'mg', 'million', 'ml', 'mug', 'na', 'nay', 'necessarily', 'nos', 'noted', 'obtain', 'obtained', 'omitted', 'ord', 'owing', 'page', 'pages', 'poorly', 'possibly', 'potentially', 'pp', 'predominantly', 'present', 'previously', 'primarily', 'promptly', 'proud', 'quickly', 'ran', 'readily', 'ref', 'refs', 'related', 'research', 'resulted', 'resulting', 'results', 'run', 'sec', 'section', 'shed', 'shes', 'showed', 'shown', 'showns', 'shows', 'significant', 'significantly', 'similar', 'similarly', 'slightly', 'somethan', 'specifically', 'state', 'states', 'stop', 'strongly', 'substantially', 'successfully', 'sufficiently', 'suggest', 'thered', 'thereof', 'therere', 'thereto', 'theyd', 'theyre', 'thou', 'thoughh', 'thousand', 'throug', 'til', 'tip', 'ts', 'ups', 'usefully', 'usefulness', "'ve", 'vol', 'vols', 'wed', 'whats', 'wheres', 'whim', 'whod', 'whos', 'widely', 'words', 'world', 'youd', 'youre']
 
-# Chargement du modèle si pas déjà chargé
+# Chargement du modèle prédictif
 if 'model' not in st.session_state:
-    url = 'https://github.com/JohannG3/DSML_EPFL_Rolex/blob/main/french_difficulty_predictor_model.joblib?raw=true'
-    response = requests.get(url)
+    model_url = 'https://github.com/JohannG3/DSML_EPFL_Rolex/blob/main/french_difficulty_predictor_model.joblib?raw=true'
+    response = requests.get(model_url)
     st.session_state.model = load(BytesIO(response.content))
 
-
-
-# Fonction pour obtenir des synonymes avec WordsAPI
-def get_synonyms(word):
-    url = f"https://wordsapiv1.p.rapidapi.com/words/{word}/synonyms"
-    headers = {
-        'x-rapidapi-host': "wordsapiv1.p.rapidapi.com",
-        'x-rapidapi-key': "864ad2ff57mshd1f224c4268230bp11ee28jsn58d9f3f8ad52"  # Replace YOUR_RAPIDAPI_KEY with your actual key from RapidAPI
-    }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data['synonyms'] if 'synonyms' in data else []
-    return []
-
-# Prédiction de la difficulté et gestion des traductions et synonymes
-sentence = st.text_input("Sentence in French", "")
-
-if st.button('Predict and Enhance'):
-    # Traduire de français à anglais
-    
-    
-    # Obtenir des synonymes en anglais
-    english_words = sentence.split()
-    synonyms = {word: get_synonyms(word) for word in english_words}
-    
-
-    
-    st.write("Synonyms translated back to French:")
-    for word, syns in synonyms.items():
-        st.write(f"{word} (EN) -> {', '.join(syns)} (FR)")
-
-    # Effectuer la prédiction de difficulté en français
-    prediction = st.session_state.model.predict([sentence])[0]
-    st.write(f"The predicted difficulty level for this sentence in French is: {prediction}")
-    """
-
-import streamlit as st
-import requests
-from joblib import load
-from io import BytesIO
-
-# Titre de l'application
-st.title('Predicting sentence difficulty in French with Synonym Enhancement')
-
-# Description
-st.write("Enter a sentence in French to predict its difficulty level, translate to English, get synonyms in English, and translate them back to French.")
-
-# Chargement du modèle si pas déjà chargé
-if 'model' not in st.session_state:
-  url = 'https://github.com/JohannG3/DSML_EPFL_Rolex/blob/main/french_difficulty_predictor_model.joblib?raw=true'
-  response = requests.get(url)
-  st.session_state.model = load(BytesIO(response.content))
-
-def translate_text(text, source_language, target_language):
-    url = "https://text-translator2.p.rapidapi.com/translate"
-    payload = {
-        "source_language": source_language,
-        "target_language": target_language,
-        "text": text
-    }
+# Fonctions auxiliaires pour la traduction et la récupération des synonymes
+def translate_text(text, source_lang, target_lang):
+    translate_url = "https://text-translator2.p.rapidapi.com/translate"
+    payload = f"source_language={source_lang}&target_language={target_lang}&text={text}"
     headers = {
         "content-type": "application/x-www-form-urlencoded",
         "X-RapidAPI-Key": "864ad2ff57mshd1f224c4268230bp11ee28jsn58d9f3f8ad52",
         "X-RapidAPI-Host": "text-translator2.p.rapidapi.com"
     }
-    response = requests.post(url, data=payload, headers=headers)
-    if response.status_code == 200:
-        result = response.json().get('data', {}).get('translatedText', '')
-        return result
-    else:
-        st.error(f"Failed to translate. Status code: {response.status_code}")
-        return None
+    response = requests.post(translate_url, data=payload, headers=headers)
+    if response.status_code == 200 and 'data' in response.json():
+        return response.json()['data']['translatedText']
+    return "Translation error"
 
-# Fonction pour obtenir des synonymes avec WordsAPI
 def get_synonyms(word):
-  url = f"https://wordsapiv1.p.rapidapi.com/words/{word}/synonyms"
-  headers = {
-    'x-rapidapi-host': "wordsapiv1.p.rapidapi.com",
-    'x-rapidapi-key': "864ad2ff57mshd1f224c4268230bp11ee28jsn58d9f3f8ad52"
-  }
-  response = requests.get(url, headers=headers)
-  if response.status_code == 200:
-    data = response.json()
-    return data['synonyms'] if 'synonyms' in data else []
-  return []
+    # Vérifiez si le mot est un stop word
+    if word.lower() in english_stopwords:
+        return ['No synonym']  # Retourne une liste vide si c'est un stop word
+    synonyms_url = f"https://wordsapiv1.p.rapidapi.com/words/{word}/synonyms"
+    headers = {
+        'x-rapidapi-host': "wordsapiv1.p.rapidapi.com",
+        'x-rapidapi-key': "864ad2ff57mshd1f224c4268230bp11ee28jsn58d9f3f8ad52"
+    }
+    response = requests.get(synonyms_url, headers=headers)
+    if response.status_code == 200 and 'synonyms' in response.json():
+        return response.json()['synonyms']
+    return ['No synonyms found']
 
-# Prédiction de la difficulté et gestion des traductions et synonymes
-sentence = st.text_input("Sentence in French", "")
+def process_sentence(sentence):
+    # Traduction de la phrase en anglais
+    english_translation = translate_text(sentence, 'fr', 'en')
+    st.write(f"The sentence you introduced means : {english_translation}")
 
-if st.button('Predict and Enhance'):
-  """
-  # Obtenir des synonymes en anglais
-  english_words = sentence.split()
-  synonyms = {word: get_synonyms(word) for word in english_words}
+# Fonction principale de l'application
+def main():
+    st.title('Improved the difficulty of a sentence in French')
 
-  # Traduire les synonymes en français
-  synonyms_in_french = {}
-  for word, syn_list in synonyms.items():
-    french_synonyms = [translate_text(syn, "fr") for syn in syn_list]
-    synonyms_in_french[word] = french_synonyms
+    if 'new_sentence' not in st.session_state:
+        st.session_state.new_sentence = ""
 
-  st.write("English synonyms and their French translations:")
-  for word, syns in synonyms_in_french.items():
-    st.write(f"{word} (EN) -> {', '.join(syns)} (FR)")
+    # Interface utilisateur
+    sentence = st.text_input("Enter a sentence in French")
+    if st.button('Analyze the sentence'):
+        # Prédiction de la difficulté
+        difficulty = st.session_state.model.predict([sentence])[0]
+        st.write(f"Predicted difficulty level for this sentence in French : {difficulty}")
+        st.session_state.difficulty = difficulty
+    
+        # Traduction et traitement des mots
+        process_sentence(sentence)
 
-  # Effectuer la prédiction de difficulté en français
-  sentence_fr = translate_text(sentence, "fr")
-  prediction = st.session_state.model.predict([sentence_fr])[0]
-  st.write(f"The predicted difficulty level for this sentence in French is: {prediction}")
-  """
-  sentence_fr = translate_text(sentence, "en", "fr")
-  st.write(f"The translated sentence in French is: {sentence_fr}")
+    st.header(f"Now, increase your vocabulary with some synonyms!")
 
+    # Processus de traduction, obtention des synonymes, et re-traduction
+    words = sentence.split()
+    synonyms_in_french = {}
+    for word in words:
+        # Traduire chaque mot en anglais
+        translated_word = translate_text(word, 'fr', 'en')
+        # Obtenir les synonymes en anglais
+        if translated_word != "Translation error":
+            synonyms = get_synonyms(translated_word)
+            # Retraduire les synonymes en français
+            translated_synonyms = [translate_text(syn, 'en', 'fr') for syn in synonyms]
+            synonyms_in_french[word] = translated_synonyms
+        else:
+            synonyms_in_french[word] = ["Erreur de traduction"]
+    
+    # Afficher les synonymes traduits pour chaque mot
+    st.write("Synonyms for each word:")
+    for word, syns in synonyms_in_french.items():
+        st.write(f"{word} : {', '.join(syns)}")
+    
+    # Demande de nouvelle phrase
+    new_sentence = st.text_input("Enter a new sentence to try to improve you french level and test your learning!", value=st.session_state.new_sentence)
+    if st.button('Submit the improved sentence', key="submit_new"):
+        new_difficulty = st.session_state.model.predict([new_sentence])[0]
+        if new_difficulty > st.session_state.difficulty:
+            st.success("Congratulations ! The difficulty level of your sentence has increased.")
+            if st.button('Start again with a new sentence', key="restart"):
+                st.session_state.clear()
+        else:
+            st.error("The difficulty level has not increased. Try again !")
+            st.session_state.new_sentence = new_sentence
+
+if __name__ == "__main__":
+    main()
